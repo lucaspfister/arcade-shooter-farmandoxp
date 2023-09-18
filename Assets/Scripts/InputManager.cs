f6917f76b39c734b63998668f4cd01b55ef808f2
lucaspfister <lucas.tyler@gmail.com>
2023-09-18 16:26:53 -0300

    02-InputSystem

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager
+{
+    public event Action OnShoot;
+
+    public Vector2 Movement => Controls.Gameplay.Movement.ReadValue<Vector2>();
+
+    private Controls Controls;
+
+    public InputManager()
+    {
+        Controls = new Controls();
+        Controls.Gameplay.Enable();
+        Controls.Gameplay.Shoot.performed += ShootPerformed;
+    }
+
+    private void ShootPerformed(InputAction.CallbackContext context)
+    {
+        OnShoot?.Invoke();
+    }
+}
